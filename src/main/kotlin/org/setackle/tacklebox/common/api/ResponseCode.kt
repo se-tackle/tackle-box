22c86c3966d56711368348cdf6b04b8ec4165fb3
jhlee <jun73045928@gmail.com>
2025-09-04 17:01:33 +0900

    feat: GlobalExceptionHandler 예외 추가

--- src/main/kotlin/org/setackle/tacklebox/common/api/ResponseCode.kt
+++ src/main/kotlin/org/setackle/tacklebox/common/api/ResponseCode.kt
@@ -7,6 +7,9 @@ enum class ResponseCode(
     SUCCESS(200, "요청 성공"),
 
     BAD_REQUEST(400, "잘못된 요청"),
-    SERVER_ERROR(500, "서버 에러")
+    NOT_FOUND(404, "요청 결과 찾을 수 없음"),
+    CONFLICT(409, "서버 충돌"),
+
+    SERVER_ERROR(500, "서버 에러"),
 
 }
